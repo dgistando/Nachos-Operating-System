518df14c03116d8114b921f8a5a2e8cccd036caa
Johnny Lo <jlo32@ucmerced.edu>
2018-03-17 23:51:32 -0700

    Implemented LotteryScheduler.java
    still missing function "pickNextThread()".
    
    need to test for test cases.

--- Nachos/src/nachos/threads/LotteryScheduler.java
+++ Nachos/src/nachos/threads/LotteryScheduler.java
@@ -27,12 +27,77 @@ import java.util.Iterator;
  * the maximum).
  */
 public class LotteryScheduler extends PriorityScheduler {
+
+    // initialize min value for lowest priority
+    public static final int PRIORITY_MINIMUM = 1;
+
+    //initialize max value for highest priority
+    public static final int PRIORITY_MAXIMUM = Integer.MAX_VALUE;   //initialize max value for highest priority
+
     /**
      * Allocate a new lottery scheduler.
      */
     public LotteryScheduler() {
+
     }
-    
+
+    public void setPriority(KThread thread, int priority) {
+        // Check to see if interrupt disabled
+        Lib.assertTrue(Machine.interrupt().disabled());
+
+        Lib.assertTrue(priority >= PRIORITY_MINIMUM);
+        // set priority
+        getThreadState(thread).setPriority(priority);
+    }
+
+    public boolean priorityIncrease() {
+        // disable interrupt
+        Machine.interrupt().disable();
+
+        KThread thread = KThread.currentThread();
+        int priority = getPriority(thread);
+        // return false if priority is equaled to max priority
+        if(priority == PRIORITY_MAXIMUM)
+            return false;
+        // set current priority to +1 higher
+        setPriority(thread, priority+1);
+
+        Machine.interrupt().enable;
+        return true;
+    }
+
+    public boolean priorityDecrease() {
+        //disable interrupt
+        Machine.interrupt().disable();
+
+        KThread thread = KThread.currentThread();
+        int priority = getpriority(thread);
+        // if priority equal min priority, return false
+        if(priority == PRIORITY_MINIMUM)
+            return false;
+        setPriority(thread, priority+1);
+
+        Machine.interrupt().enable();
+        return true;
+    }
+
+    public int getEffectivePriority(ThreadState mystate) {
+        int ticket = mystate.priority;
+
+        for(PriorityQueue myQueue: mystate.donateQueue){
+            for (KThread currentThread : myQueue.waitQueue) {
+                if(getThreadState(currentThread) == mystate)
+                    continue;
+                tickets += getThreadState(currentThread).getEffectivePriority();
+            }
+        }
+        return tickets;
+    }
+
+    public void pickNextThread() {
+        // implement me
+    }
+
     /**
      * Allocate a new lottery thread queue.
      *
@@ -41,8 +106,10 @@ public class LotteryScheduler extends PriorityScheduler {
      *					to the owning thread.
      * @return	a new lottery thread queue.
      */
+    /**
     public ThreadQueue newThreadQueue(boolean transferPriority) {
 	// implement me
 	return null;
     }
+    */
 }
